4db3751999a1445eb66a1fb6fc0988f1de45c108
yotamlevit <yotam.levit@gmail.com>
2024-07-20 16:37:52 +0300

    Move utils function to utils

--- include/utils.h
+++ include/utils.h
@@ -5,8 +5,15 @@
 #ifndef ASSEMBLER_SEMICOMPILER_UTILS_H
 #define ASSEMBLER_SEMICOMPILER_UTILS_H
 
+#include "globals.h"
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 FILE* open_file(char* file);
+
+int is_operation(char*);
+
+int is_stop(char*);
 
 #endif //ASSEMBLER_SEMICOMPILER_UTILS_H
