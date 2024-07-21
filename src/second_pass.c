34613295ac1208d91b816947494d1f2fdf1b5631
yotamlevit <yotam.levit@gmail.com>
2024-07-21 15:46:32 +0300

    Reorder functions by call

--- src/second_pass.c
+++ src/second_pass.c
@@ -26,137 +26,6 @@ int find_symbol(const char* line, const char* cmp_str, int length) {
 //void update_machine_word
 
 
-void analize_2_second_pass(char* l)
-{
-    /*l1 is a point in the first place right after the first spaces*/
-    char* l1 = delete_first_spaces(l);
-    /*This condition continues the text transition as long as the line is blank or consist comments*/
-    if (!strncmp(l1, ".entry", strlen(".entry")))
-    {
-        process_entry(l1 + 6);
-        return;
-    }
-    if (isLabel2(l1))
-    {
-        process_lable(l1);
-        return;
-    }
-    if (is_operation(l1))
-    {
-        second_operation(l1 + OPERATION_LENGTH);
-        return;
-    }
-    if (is_stop(l1))
-    {
-        second_operation(l1 + STOP_LENGTH);
-        return;
-    }
-}
-
-int second_pass_exec(FILE* file_handle)
-{
-    fd = file_handle;
-    /*Second pass*/
-    while (!feof(fd))
-    {
-        /*Second analize*/
-        analize_2_second_pass(line);
-        line_counter++;
-        /*Get one line from the file V */
-        fgets(line, MAX_LINE_LENGTH, fd);
-    }
-
-    return validate_second_pass();
-}
-
-
-int isLabel2(char* line)
-{
-	int i;
-	line = delete_first_spaces(line);
-	for (i = 0; i < MAX_LINE_LENGTH; i++)
-	{
-		if (line[i] == ':')
-			return 1;
-	}
-	return 0;
-}
-
-/*This function creates a list that handles the entry guides*/
-void process_entry(char* li)
-{
-	symbol* temp = head_symbol;
-	symbol* temp2;
-	boolean is_exist = NO;
-	int i;
-	li = delete_first_spaces(li);
-	if (*li == '\0')
-	{
-		printf("ERROR!! line %d: Label is Missing\n", line_counter);
-		error_flag = ON;
-	}
-	if (strlen(li) > MAX_LABEL_LENGTH)
-	{
-		printf("ERROR!! line %d: Label is too long\n", line_counter);
-		error_flag = ON;
-	}
-	for (i = 1; i < MAX_LINE_LENGTH || li[i] == '\0'; i++)
-	{
-		if (li[i] == ' ' || li[i] == '\n' || li[i] == '\t')
-			break;
-	}
-	while (temp)
-	{
-		if (!strncmp(li, temp->symbol_name, i))
-		{
-			is_exist = YES;
-			if (temp->is_external)
-			{
-				printf("ERROR!! line %d: This label is external\n", line_counter);
-				error_flag = ON;
-			}
-			else
-			{
-				/*Create a list of entry labels*/
-				temp2 = (symbol*)malloc(sizeof(symbol));
-				if (!temp2)
-				{
-					printf("No memmory\n");
-					break;
-				}
-				temp2->next = head_entries;
-				head_entries = temp2;
-				clean_label_name(head_entries->symbol_name);
-				head_entries->address = temp->address;
-				strcpy(head_entries->symbol_name, temp->symbol_name);
-			}
-		}
-		temp = temp->next;
-	}
-	if (is_exist == NO)
-	{
-		printf("ERROR!! line %d: Label is not exist\n", line_counter);
-		error_flag = ON;
-	}
-}
-
-
-/*This function is called if the word is a label*/
-void process_lable(char* line)
-{
-	int i;
-	for (i = 0; i < MAX_LINE_LENGTH; i++)
-	{
-		if (line[i] == ':')
-		{
-			/*go again to analize2 */
-			analize_2_second_pass(line + i + 1);
-			return;
-		}
-	}
-}
-
-
 void handle_method_destination(char* li, code_word_fields_ptr code_word, machine_word_fields_ptr dest_machine_word) {
     symbol* temp;
     symbol* ext;
@@ -332,4 +201,137 @@ void second_operation(char* li)
 	{
         handle_two_operands(li);
 	}
+}
+
+
+int isLabel2(char* line)
+{
+    int i;
+    line = delete_first_spaces(line);
+    for (i = 0; i < MAX_LINE_LENGTH; i++)
+    {
+        if (line[i] == ':')
+            return 1;
+    }
+    return 0;
+}
+
+
+/*This function is called if the word is a label*/
+void process_lable(char* line)
+{
+    int i;
+    for (i = 0; i < MAX_LINE_LENGTH; i++)
+    {
+        if (line[i] == ':')
+        {
+            /*go again to analize2 */
+            analize_2_second_pass(line + i + 1);
+            return;
+        }
+    }
+}
+
+
+/*This function creates a list that handles the entry guides*/
+void process_entry(char* li)
+{
+    symbol* temp = head_symbol;
+    symbol* temp2;
+    boolean is_exist = NO;
+    int i;
+    li = delete_first_spaces(li);
+    if (*li == '\0')
+    {
+        printf("ERROR!! line %d: Label is Missing\n", line_counter);
+        error_flag = ON;
+    }
+    if (strlen(li) > MAX_LABEL_LENGTH)
+    {
+        printf("ERROR!! line %d: Label is too long\n", line_counter);
+        error_flag = ON;
+    }
+    for (i = 1; i < MAX_LINE_LENGTH || li[i] == '\0'; i++)
+    {
+        if (li[i] == ' ' || li[i] == '\n' || li[i] == '\t')
+            break;
+    }
+    while (temp)
+    {
+        if (!strncmp(li, temp->symbol_name, i))
+        {
+            is_exist = YES;
+            if (temp->is_external)
+            {
+                printf("ERROR!! line %d: This label is external\n", line_counter);
+                error_flag = ON;
+            }
+            else
+            {
+                /*Create a list of entry labels*/
+                temp2 = (symbol*)malloc(sizeof(symbol));
+                if (!temp2)
+                {
+                    printf("No memmory\n");
+                    break;
+                }
+                temp2->next = head_entries;
+                head_entries = temp2;
+                clean_label_name(head_entries->symbol_name);
+                head_entries->address = temp->address;
+                strcpy(head_entries->symbol_name, temp->symbol_name);
+            }
+        }
+        temp = temp->next;
+    }
+    if (is_exist == NO)
+    {
+        printf("ERROR!! line %d: Label is not exist\n", line_counter);
+        error_flag = ON;
+    }
+}
+
+
+void analize_2_second_pass(char* l)
+{
+    /*l1 is a point in the first place right after the first spaces*/
+    char* l1 = delete_first_spaces(l);
+    /*This condition continues the text transition as long as the line is blank or consist comments*/
+    if (!strncmp(l1, ".entry", strlen(".entry")))
+    {
+        process_entry(l1 + 6);
+        return;
+    }
+    if (isLabel2(l1))
+    {
+        process_lable(l1);
+        return;
+    }
+    if (is_operation(l1))
+    {
+        second_operation(l1 + OPERATION_LENGTH);
+        return;
+    }
+    if (is_stop(l1))
+    {
+        second_operation(l1 + STOP_LENGTH);
+        return;
+    }
+}
+
+
+int second_pass_exec(FILE* file_handle)
+{
+    fd = file_handle;
+    /*Second pass*/
+    while (!feof(fd))
+    {
+        /*Second analize*/
+        analize_2_second_pass(line);
+        line_counter++;
+        /*Get one line from the file V */
+        fgets(line, MAX_LINE_LENGTH, fd);
+    }
+
+    return validate_second_pass();
 }
